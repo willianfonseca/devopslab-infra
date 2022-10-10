71d74ccb89093dc9727f40ae6752e16b765e50d3
willianfonseca <98983728+willianfonseca@users.noreply.github.com>
2022-10-10 17:04:16 -0300

    adding basic structure of code for app

---
+++ gcp-main.tf
@@ -0,0 +1,11 @@
+resource "google_app_engine_application" "app" {
+  project     = "lab-devops-8aso-willianfonseca"
+  location_id = "us-central"
+}
+
+resource "google_artifact_registry_repository" "my-repo" {
+  location = "us-central1"
+  repository_id = "labdevops"
+  description = "Imagens Docker"
+  format = "DOCKER"
+}
---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.34.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {  
+  project = "lab-devops-8aso-willianfonseca"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
